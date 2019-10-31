1fa230529ce51cf820a46a594cf1107a9e00aa24
Ruben Dario <kilerleper@gmail.com>
2019-10-30 22:04:08 -0500

    Se realiza proyecto de front y back
    Primer front y back

---
+++ app_onstep_backend/src/main/java/com/app/onstep/OnstepApplication.java
@@ -0,0 +1,13 @@
+package com.app.onstep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnstepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnstepApplication.class, args);
+	}
+
+}
